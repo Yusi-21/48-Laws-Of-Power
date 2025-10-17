a3c92c5e6c7d142e70ed6abbc0715164e89b0158
VuDucZuy <duczuyvu12@gmail.com>
2025-10-17 22:08:18 +0300

    Change Header text size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,9 @@ const Header: React.FC<HeaderProps> = ({ darkMode, setDarkMode }) => {
       <div className="max-w-2xl mx-auto px-4 py-4 flex items-center justify-between">
         <div className="flex items-center gap-3">
           <div className="bg-slate-800 p-1">
-            <img src="/src/images/icon_law.png" width={30} height={30}></img>
+            <img src="/src/images/icon_law.png" width={50} height={50}></img>
           </div>
-          <h1 className={`text-xl font-bold ${darkMode ? 'text-white' : 'text-slate-900'}`}>
+          <h1 className={`text-4xl font-bold ${darkMode ? 'text-white' : 'text-slate-900'}`}>
             {lawsStrings.length} Laws Of Power
           </h1>
         </div>
