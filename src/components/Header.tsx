d0b5b2afdf18cd9879c4a84d93b9797a56100ce8
VuDucZuy <duczuyvu12@gmail.com>
2025-10-17 17:00:32 +0300

    Change favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC<HeaderProps> = ({ darkMode, setDarkMode }) => {
       <div className="max-w-2xl mx-auto px-4 py-4 flex items-center justify-between">
         <div className="flex items-center gap-3">
           <div className="bg-slate-800 p-1">
-            <img src="/icon_law.png" width={30} height={30}></img>
+            <img src="/src/images/icon_law.png" width={30} height={30}></img>
           </div>
           <h1 className={`text-xl font-bold ${darkMode ? 'text-white' : 'text-slate-900'}`}>
             {lawsStrings.length} Laws Of Power
